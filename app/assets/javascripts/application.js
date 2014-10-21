e28de4073dfa7709cdcf0fc576a4d2b803c3a332
Holly Moody <hollymoody@charter.net>
2014-10-21 09:54:01 -0400

    Initial in-place editing of the character fields.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require turbolinks
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
-//= require turbolinks
+//= require jquery.purr
+//= require best_in_place
 //= require_tree .
+
