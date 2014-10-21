cb26ee5cd9d9f269dd5c24edcd4dee317e515bd4
Holly Moody <hollymoody@charter.net>
2014-10-21 14:36:02 -0400

    Consolidate character listing to one partial (again). Make fields editable if belong to existing user and user logged in. Let user know if a sheet belongs to them

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index, :license]
 
   def index
-    @characters = Character.where("public_flag = ?", false)
+    @characters = Character.where("public_flag = ?", true)
   end
 
   def license
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def character_editable?(char)
+    return ((user_signed_in?) && (current_user.id == char.user_id))
+  end
+
 end
