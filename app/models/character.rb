0817e329c281d8a68c3647c168391e0b01404a0f
Holly Moody <hollymoody@charter.net>
2014-10-23 04:23:24 -0400

    Add dependent destroy to the user and character models.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ActiveRecord::Base
   belongs_to :user
-  has_many :character_classes
+  has_many :character_classes, dependent: :destroy
 
   validates :user_id, presence: true
 end
